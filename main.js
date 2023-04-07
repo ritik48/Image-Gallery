f5b4a7ca2016a4ce5fdf2735624dff27911ab67d
Ritik <raj769417@gmail.com>
2023-04-07 15:10:40 +0530

    changed formatting

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const container = document.querySelector('.container');
-      const popup=document.querySelector('.popup');
-      const close_btn=popup.querySelector('button');
-      
-      let category=['india','london','russia','arctic','europe','arabia','switzerland','oceans','finland','thailand','dessert','mexico',
-                      'brazil', 'usa', 'canada','indonesia','japan','italy','malaysia'];
-  
-      const orient=['vtall','htall']
-  
-      function get_category() {
-        return category[Math.floor(Math.random() * category.length())]; 
-      }
-      
-  
-      function get_image_html(cat, orient) {
-          return `
+const popup = document.querySelector('.popup');
+const close_btn = popup.querySelector('button');
+
+let category = ['india', 'london', 'russia', 'arctic', 'europe', 'arabia', 'switzerland', 'oceans', 'finland', 'thailand', 'dessert', 'mexico',
+  'brazil', 'usa', 'canada', 'indonesia', 'japan', 'italy', 'malaysia'];
+
+const orient = ['vtall', 'htall']
+
+function get_category() {
+  return category[Math.floor(Math.random() * category.length())];
+}
+
+
+function get_image_html(cat, orient) {
+  return `
             <div class="img-cont ${orient}">
                 <img src="https://source.unsplash.com/random/?${cat}" alt="">
                 <div class="overlay">
@@ -21,25 +21,27 @@ const container = document.querySelector('.container');
                 </div>
             </div>
           `;
-      }
-      let all_img='';
-      category.forEach((cat) => {
-          const orientation = orient[Math.floor(Math.random() * 2)];
-          const img_html=get_image_html(cat, orientation);
-          all_img+=img_html;
-      })
-      container.innerHTML=all_img;
-      const view_btn=document.querySelectorAll('.view');
-      view_btn.forEach((btn) => {
-        btn.addEventListener('click', show_popup);
-      })
+}
+
+let all_img = '';
+category.forEach((cat) => {
+  const orientation = orient[Math.floor(Math.random() * 2)];
+  const img_html = get_image_html(cat, orientation);
+  all_img += img_html;
+})
+container.innerHTML = all_img;
+
+const view_btn = document.querySelectorAll('.view');
+view_btn.forEach((btn) => {
+  btn.addEventListener('click', show_popup);
+})
 
 
-      function show_popup(e) {
-        popup.classList.remove('close');
-        const src=e.target.parentElement.parentElement.querySelector('img').src;
-        popup.querySelector('img').src=src;
-      }
-      close_btn.addEventListener('click', () => {
-        popup.classList.add('close');
-      })
+function show_popup(e) {
+  popup.classList.remove('close');
+  const src = e.target.parentElement.parentElement.querySelector('img').src;
+  popup.querySelector('img').src = src;
+}
+close_btn.addEventListener('click', () => {
+  popup.classList.add('close');
+})
